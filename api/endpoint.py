33ccd0ba29b7dc5edfe88ca60b1e75de38aab069
Luiz Henrique Pereira Niero <luiz_niero@hotmail.com>
2023-09-10 20:12:26 -0300

    Endpoint que retorna livros ou erro.

---
+++ api/endpoint.py
@@ -0,0 +1,14 @@
+from flask import Flask, abort
+
+app = Flask(__name__)
+
+@app.route('/api/<tipo>', methods=['GET'])
+def index(tipo):
+    print(f"Requisição de lista do tipo: {tipo}")
+    if tipo.lower() == 'livros':
+       return ["Dom Casmurro", "Harry Potter e a criança amaldiçoada", "O Caibalion"]
+    else:
+       abort(403, f"O tipo {tipo} não é permitido neste endpoint.")
+
+if __name__ == '__main__':
+ app.run(debug=True, port=5001)
